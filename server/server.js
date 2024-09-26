6f550fba6f3e6486ebfc7b9dac87b7bfbe55e334
Chaim Gluck <chaimshglick@gmail.com>
2024-09-26 12:50:19 -0400

    Initial commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
+app.use(cors());
 
 // Serve static files from the React app
 // const publicPath = path.join(__dirname, '../client/build');
