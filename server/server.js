e28eb0b406b8ee0f20d8a7514efb04016ce6335e
Chaim Gluck <chaimshglick@gmail.com>
2024-09-26 19:28:57 -0400

    Initial commit

--- server/server.js
+++ server/server.js
@@ -5,16 +5,19 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log('filename:', __filename);
-console.log('dir name:', __dirname)
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://shark-app-lstbh.ondigitalocean.app/'
+    origin: 'https://shark-app-lstbh.ondigitalocean.app'
 }));
+
+// For any other routes, serve the React front-end
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Serve static files from the React app
 const publicPath = path.join(__dirname, '../client/build');
@@ -23,11 +26,6 @@ app.use(express.static(publicPath));
 // Simple API route
 app.get('/api/message', (req, res) => {
     res.json({ message: "Hello from the back-end!" });
-});
-
-// For any other routes, serve the React front-end
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Start server
