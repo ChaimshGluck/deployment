39a5d421c4b8ebd0f00b0aae638df30d9083e984
Chaim Gluck <chaimshglick@gmail.com>
2024-09-29 13:19:30 -0400

    Initial commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
